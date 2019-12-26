cbf0e14567ca5c5d8f6ca9aa14b08f0e7af2ee5e
vokda <djing_djang@hotmail.com>
2019-12-26 23:13:39 +0100

    Bug fix
    If only one version of firefox exists in directory then
    latest_version.pl would return nothing. This is now fixed.

--- latest_version.pl
+++ latest_version.pl
@@ -1,13 +1,26 @@
 #!/usr/bin/perl
+# use Data::Dumper;
+
+# if only one version exsits assume it's the latest
+if(keys @ARGV == 1)
+{
+	print $ARGV[0];
+	exit;
+}
 
 # obtain latest version of firefox tar ball
 
+$debug = 0;
 %tar_balls;
 for my $tar_ball (@ARGV)
 {
-	$tar_ball =~ m/(\d+)\.(\d+)\.(\d+)/;
+	$tar_ball =~ m/(\d+)\.(\d+)\.(\d+)?/;
 	$tar_balls{$tar_ball} = [$1, $2, $3];
 }
+
+
+# print STDERR Dumper \%tar_balls if $debug;
+
 
 # compare version numbers, first $1 then $2 then $3
 
@@ -15,6 +28,8 @@ $hi = 0; # highest index
 $eq = 0; # multiple high ones
 $v = 0;
 @keys = keys %tar_balls;
+# print STDERR Dumper \@keys if $debug;
+
 do
 {
 	$eq = 0;
